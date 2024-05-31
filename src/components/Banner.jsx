e0b5ae52116eee9e442e8d3f9e16ff189bf8cf02
RakshitChaurasia <139792069+rakshithhub@users.noreply.github.com>
2024-06-01 00:10:48 +0530

    change in Banner file Button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import { FaArrowRight } from "react-icons/fa6";
 import Profile from "../../src/assets/myImages/profile.jpg"
+import {Link} from "react-scroll";
 
 const Banner = () => {
 
@@ -11,7 +12,7 @@ const Banner = () => {
                     <h1 className="text-2xl sm:text-5xl lg:text-6xl uppercase font-bold w-full mb-5 text-gray-500 mt-16">full stack developer</h1>
                     <p className="mb-5 text-gray-300 text-[20px] md:text-2xl capitalize">Hi, I'm <b style={{"textShadow": "2px 2px 2px gray"}}>Rakshit Chaurasia</b>. And i'm a software engineer</p>
                     <button className="group px-2 py-3 sm:px-3 sm:py-4 uppercase font-bold bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg cursor-pointer hover:scale-105 duration-300 flex justify-center items-center">
-                        portfolio <FaArrowRight className="ml-3 group-hover:rotate-90 duration-200"/>
+                        <Link to="portfolio" smooth duration={500}>portfolio</Link> <FaArrowRight className="ml-3 group-hover:rotate-90 duration-200"/>
                     </button>
                 </div> 
                 <div className="text-white rounded-lg w-full">
